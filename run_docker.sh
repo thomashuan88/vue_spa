bf25e9197478404429edbd4a1f24e296a177c1de
thomas <thomas@localhost.localdomain>
2019-12-18 21:29:19 -0500

    first commit

---
+++ run_docker.sh
@@ -0,0 +1,2 @@
+docker run -itd --network=host --name=vue_spa --user 1000:1000 -v $(pwd):/home/node/vue_spa thomashuan88/vue_spa npm run dev
+
---
+++ run_docker_exec.sh
@@ -0,0 +1,2 @@
+docker exec -it -w=/home/node/vue_spa vue_spa bash
+
